499f48d6a7a9d3dcf90c712f5fcb97999c0d9e81
GiuseppeTimpano <giuseppe.timpano@unicz.it>
2026-02-19 13:40:07 +0100

    lora model adpted for linear lora layers

--- src_torch/dafne_models/core/lora/lora_models.py
+++ src_torch/dafne_models/core/lora/lora_models.py
@@ -1,6 +1,8 @@
 import re 
+import copy
+
 from torch import nn, Tensor
-from .layers import LoRA2dConvLayer, LoRA3dConvLayer, LoRANdConvLayer
+from .layers import LoRA2dConvLayer, LoRA3dConvLayer, LoRANdConvLayer, LoRALinearLayer
 
 class LoRAModel(nn.Module):
     def __init__(self, base_model:nn.Module, lora_config:dict) -> None: 
@@ -18,34 +20,43 @@ class LoRAModel(nn.Module):
         for target_module_name, config in lora_config.items():
             for module_name, module in self.base_model.named_modules():
                 if re.match(target_module_name, module_name):
-                    self.lora_module_names.append(module_name)
                     if isinstance(module, nn.modules.conv._ConvNd):
+                        self.lora_module_names.append(module_name)
                         lora_module = LoRANdConvLayer(module, config)
+                        self._recursive_setattr(self.base_model, module_name, lora_module)
+                    elif isinstance(module, nn.Linear):
+                        self.lora_module_names.append(module_name)
+                        lora_module = LoRALinearLayer(module, config)
+                        self._recursive_setattr(self.base_model, module_name, lora_module)
                     else: 
-                        raise AssertionError('Invalid Target Module Type! Only Conv layers are supported')
-                    setattr(self.base_model, module_name, lora_module)
+                        raise AssertionError('Invalid Target Module Type! Only Conv and Linear layers are supported')
 
+    def _recursive_setattr(self, obj, attr_path, value):
+        parts = attr_path.split('.')
+        for part in parts[:-1]:
+            obj = getattr(obj, part)
+        setattr(obj, parts[-1], value)
+    
     def enable_adapter(self) -> None:
         for lora_module_name in self.lora_module_names:
-            getattr(self.base_model, lora_module_name).enable_adapter()
+            lora_module = self.base_model.get_submodule(lora_module_name)
+            lora_module.enable_adapter()
     
     def disable_adapter(self) -> None:
         for lora_module_name in self.lora_module_names:
-            getattr(self.base_model, lora_module_name).disable_adapter()
+            lora_module = self.base_model.get_submodule(lora_module_name)
+            lora_module.disable_adapter()
     
     def forward(self, x:Tensor) -> Tensor:
         return self.base_model(x)
     
     def get_merged_model(self) -> nn.Module:
-        merged_model = self.base_model.__class__()
-        for module_name, module in merged_model.named_modules():
-            if module_name == '':
-                continue
-            
-            if module_name in self.lora_module_names:
-                setattr(merged_model, module_name, getattr(self.base_model, module_name).get_merged_modules())
-            else:
-                setattr(merged_model, module_name, getattr(self.base_model, module_name))
-            
+        merged_model = copy.deepcopy(self.base_model)
+
+        for module_name in self.lora_module_names:
+            lora_module = self.base_model.get_submodule(module_name) #get submodule where lora layer is present
+            merged_module = lora_module._get_merged_modules()
+            self._recursive_setattr(merged_model, module_name, merged_module)
+        
         return merged_model
                 
